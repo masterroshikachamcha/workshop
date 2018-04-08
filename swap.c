151a1b5648d0ab37d589b26f1ab01075f3cafa60
Call Kato <masterroshikachamcha@gmail.com>
2018-04-08 15:36:41 +0000

    added a sub function

--- swap.c
+++ swap.c
@@ -4,7 +4,24 @@ void add(int a,int b){
 
     int sum=a+b;
     printf("sum is %d\n",sum);
+
+
+
+
+
 }
+
+void sub(int c , int d){
+    int diff= c-d;
+    printf("Difference is %d\n",diff);
+
+}
+
+
+
+
+
+
 int main()
 {
 
@@ -21,6 +38,8 @@ int main()
     y = temp;
 
     add(12,15);
+    sub(23,12);
+
 
     printf("x is now %d\n", x);
     printf("y is now %d\n", y);
